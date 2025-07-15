eb08c29a9307d6613eb57355be466b764d63594b
Fayzulla Raxmatullayev <fayzullarakhmatullayev@gmail.com>
2025-07-15 14:32:20 +0500

    feat: implement authentication screens and components with Appwrite integration

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,13 +1,30 @@
+import { images } from '@/constants';
 import { Slot } from 'expo-router';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Dimensions, Image, KeyboardAvoidingView, Platform, ScrollView, View } from 'react-native';
 
 const AuthLayout = () => {
   return (
-    <View>
-      <Text>AuthLayout</Text>
-      <Slot />
-    </View>
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <ScrollView className="bg-white h-full" keyboardShouldPersistTaps="handled">
+        <View
+          className="w-full relative"
+          style={{ height: Dimensions.get('screen').height / 2.25 }}
+        >
+          <Image
+            source={images.loginGraphic}
+            className="size-full rounded-b-lg"
+            resizeMode="stretch"
+          />
+          <Image
+            source={images.logo}
+            className="self-center size-48 absolute -bottom-16 z-10"
+            resizeMode="stretch"
+          />
+          <Slot />
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,13 +1,59 @@
-import { useRouter } from 'expo-router';
-import React from 'react';
-import { Button, Text, View } from 'react-native';
+import CustomButton from '@/components/CustomButton';
+import CustomInput from '@/components/CustomInput';
+import { Link, useRouter } from 'expo-router';
+import React, { useState } from 'react';
+import { Alert, Text, View } from 'react-native';
 
 const SignUp = () => {
   const router = useRouter();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [form, setForm] = useState({ name: '', email: '', password: '' });
+
+  const submit = () => {
+    if (!form.email || !form.password || !form.name)
+      return Alert.alert('Error', 'Please fill all the fields');
+    setIsSubmitting(true);
+
+    try {
+      // Appwrite sign in
+      Alert.alert('Success', 'Sign up successful');
+      setIsSubmitting(false);
+
+      router.replace('/');
+    } catch (err: any) {
+      console.log(err);
+      Alert.alert('Error', err.message);
+    }
+  };
   return (
-    <View>
-      <Text>SignUp</Text>
-      <Button title="Sign in" onPress={() => router.push('/sign-in')}></Button>
+    <View className="gap-10 bg-white rounded-lg p-5 mt-5">
+      <CustomInput
+        placeholder="Enter your full name"
+        value={form.name}
+        onChangeText={(text) => setForm({ ...form, name: text })}
+        label="Full name"
+      />
+      <CustomInput
+        placeholder="Enter your email"
+        value={form.email}
+        onChangeText={(text) => setForm({ ...form, email: text })}
+        label="Email"
+        keyboardType="email-address"
+      />
+      <CustomInput
+        placeholder="Enter your password"
+        value={form.password}
+        onChangeText={(text) => setForm({ ...form, password: text })}
+        label="Password"
+        secureTextEntry={true}
+      />
+      <CustomButton title="Sign Up" isLoading={isSubmitting} onPress={submit} />
+      <View className="flex justify-center mt-5 flex-row gap-2">
+        <Text className="base-regular text-gray-100">Already have an account?</Text>
+        <Link href="/sign-in" className="base-bold text-primary">
+          Sign In
+        </Link>
+      </View>
     </View>
   );
 };
