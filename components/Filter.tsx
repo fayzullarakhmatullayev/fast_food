b6de674a2a61fa28cb6ec382f4ebf0d7db95aa1d
Fayzulla Raxmatullayev <fayzullarakhmatullayev@gmail.com>
2025-07-16 16:51:34 +0500

    feat: search input & filter

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -1,13 +1,19 @@
 import { Category } from '@/type';
+import cn from 'clsx';
+import { router } from 'expo-router';
 import { useLocalSearchParams } from 'expo-router/build/hooks';
 import React, { useState } from 'react';
-import { FlatList, Text, TouchableOpacity } from 'react-native';
-
+import { FlatList, Platform, Text, TouchableOpacity } from 'react-native';
 const Filter = ({ categories }: { categories: Category[] }) => {
   const searchParams = useLocalSearchParams();
-  const [active, setActive] = useState(searchParams.category || '');
+  const [active, setActive] = useState(searchParams.category || 'all');
 
-  const handlePress = (id: string) => {};
+  const handlePress = (id: string) => {
+    setActive(id);
+
+    if (id === 'all') router.setParams({ category: undefined });
+    else router.setParams({ category: id });
+  };
 
   const filterData: (Category | { $id: string; name: string })[] = categories
     ? [{ $id: 'all', name: 'All' }, ...categories]
@@ -16,12 +22,22 @@ const Filter = ({ categories }: { categories: Category[] }) => {
   return (
     <FlatList
       data={filterData}
+      keyExtractor={(item) => item.$id}
+      horizontal={true}
+      showsHorizontalScrollIndicator={false}
+      contentContainerClassName="gap-x-2 pb-3"
       renderItem={({ item }) => (
-        <TouchableOpacity>
-          <Text>{item.name}</Text>
+        <TouchableOpacity
+          key={item.$id}
+          className={cn('filter mt-1 mx-1', active === item.$id ? 'bg-amber-500' : 'bg-white')}
+          style={Platform.OS === 'android' ? { elevation: 5, shadowColor: '#878787' } : {}}
+          onPress={() => handlePress(item.$id)}
+        >
+          <Text className={cn('body-medium', active === item.$id ? 'text-white' : 'text-gray-200')}>
+            {item.name}
+          </Text>
         </TouchableOpacity>
       )}
-      keyExtractor={(item) => item.$id}
     />
   );
 };
